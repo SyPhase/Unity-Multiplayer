f8eeca0c3869d111cc094e703dd68e59719de405
SyPhase <pi2568gamerz@gmail.com>
2024-09-01 18:48:02 -0600

    Completed Section 2 Lecture 28: Auth Improvements

--- Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
+++ Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
 
 public enum AuthState
 {
@@ -18,27 +20,69 @@ public static class AuthenticationWrapper
 
     public static async Task<AuthState> DoAuth(int maxTries = 5)
     {
-        if (AuthState == AuthState.Authenticated)
+        if (AuthState == AuthState.Authenticated) // Already Authenticated successfully
         {
             return AuthState;
         }
 
+        if (AuthState == AuthState.Authenticating) // Aleady started Authenticating...
+        {
+            Debug.LogWarning("Aleady authenticating, waiting for authentication...");
+            await Authenticating();
+            return AuthState;
+        }
+
+        await SignInAnonymouslyAsync(maxTries); // Begin Authentication
+
+        return AuthState;
+    }
+
+    static async Task<AuthState> Authenticating() // Wait for authentication to finish
+    {
+        while (AuthState == AuthState.Authenticating || AuthState == AuthState.NotAuthenticated)
+        {
+            await Task.Delay(1000); // Wait a second
+        }
+
+        return AuthState;
+    }
+
+    static async Task SignInAnonymouslyAsync(int maxTries)
+    {
         AuthState = AuthState.Authenticating;
+
         int tries = 0;
         while (AuthState == AuthState.Authenticating && tries < maxTries)
         {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync(); // Sign in types are available, using anon to not require sign in
-
-            if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized) // Sign in was successful
+            try
             {
-                AuthState = AuthState.Authenticated;
-                break;
+                await AuthenticationService.Instance.SignInAnonymouslyAsync(); // Sign in types are available, using anon to not require sign in
+
+                if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized) // Sign in was successful
+                {
+                    AuthState = AuthState.Authenticated;
+                    break;
+                }
+            }
+            catch (AuthenticationException ex)
+            {
+                Debug.LogError(ex);
+                AuthState = AuthState.Error;
+            }
+            catch (RequestFailedException ex)
+            {
+                Debug.LogError(ex);
+                AuthState = AuthState.Error;
             }
 
             tries++;
             await Task.Delay(2000); // Wait one second before retrying
         }
 
-        return AuthState;
+        if (AuthState != AuthState.Authenticated)
+        {
+            Debug.LogWarning($"Player was not signed in after {maxTries} tries...");
+            AuthState = AuthState.TimeOut;
+        }
     }
 }
