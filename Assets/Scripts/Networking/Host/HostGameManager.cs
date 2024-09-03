dc57f075adc4c16db953c2ac7daa68b2aab6bc4a
SyPhase <pi2568gamerz@gmail.com>
2024-09-02 22:37:20 -0600

    Completed Section 2 Lecture 33: Creating Lobbies

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -12,8 +15,9 @@ public class HostGameManager
 {
     Allocation allocation;
     string joinCode;
+    string lobbyId;
 
-    const int MaxConnections = 20;
+    const int MaxConnections = 20; // Max Players Connected
     const string GameSceneName = "Game"; // name of main game scene (currently 'Game')
 
     public async Task StartHostAsync()
@@ -45,9 +49,49 @@ public class HostGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false; // Sets lobby to public (visible in lobby list), could be set by UI button "Private Game" to require join code
+            lobbyOptions.Data = new System.Collections.Generic.Dictionary<string, DataObject>() // Creates a dictionary to give join code
+            {
+                {
+                    "JoinCode", new DataObject
+                    (
+                        visibility: DataObject.VisibilityOptions.Member, // Member means only those who join the lobby can get the join code
+                        value: joinCode
+                    )
+                }
+            };
+
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync("My Lobby", MaxConnections, lobbyOptions);
+
+            lobbyId = lobby.Id;
+
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15));
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogWarning(e);
+            return;
+        }
+
+        // Begin Hosting game
         NetworkManager.Singleton.StartHost();
 
         // Load next scene
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
+    }
+
+    IEnumerator HeartBeatLobby(float waitTimeSeconds)
+    {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+
+        while (true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+
+            yield return delay;
+        }
     }
 }
