6165673284882dfbe573ba98a3a602b546570997
SyPhase <pi2568gamerz@gmail.com>
2024-09-23 22:18:07 -0600

    Completed Section 2 Lecture 35: Player Name Selection
    Used PlayerPrefs to store name (good for storing settings, not game saves)

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameSelector : MonoBehaviour
+{
+    [SerializeField] TMP_InputField nameField;
+    [SerializeField] Button connectButton;
+    [SerializeField] int minNameLength = 1;
+    [SerializeField] int maxNameLength = 16;
+
+    const string PlayerNameKey = "PlayerName";
+
+    void Start()
+    {
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null) // is headless server
+        {
+            SceneManager.LoadScene(1 + SceneManager.GetActiveScene().buildIndex);
+            return;
+        }
+
+        nameField.text = PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+        HandleNameChanged();
+    }
+
+    public void HandleNameChanged()
+    {
+        connectButton.interactable = 
+            nameField.text.Length >= minNameLength && 
+            nameField.text.Length <= maxNameLength;
+    }
+
+    public void Connect()
+    {
+        PlayerPrefs.SetString(PlayerNameKey, nameField.text);
+
+        SceneManager.LoadScene(1 + SceneManager.GetActiveScene().buildIndex);
+    }
+}
